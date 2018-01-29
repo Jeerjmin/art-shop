b0e14ee5ea4ba8a808a671f2ef55892b6e8a7ad0
Oleg Tarasov <olegtarasov@MacBook-Pro-Oleg.local>
2018-01-29 17:36:14 +0300

    delete test

--- app/reducers/products/tests/products.spec.js
+++
@@ -1,8 +0,0 @@
-import { ProductsReducer } from '../src/products';
-import products from '../../../api/products.json';
-
-describe('ProductsReducer', () => {
-    it('should return the products', () => {
-        expect(ProductsReducer()).toEqual(products);
-    });
-});
